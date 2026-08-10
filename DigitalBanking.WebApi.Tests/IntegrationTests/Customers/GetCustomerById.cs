5f93b4b5fb08617166ee4f0a0dd234219d82f2b5
yashjaiswal <yashjaiswal985@gmail.com>
2026-08-11 01:32:07 +0530

    Started writing integration test

---
+++ DigitalBanking.WebApi.Tests/IntegrationTests/Customers/GetCustomerById.cs
@@ -0,0 +1,30 @@
+﻿using DigitalBanking.Domain.Entities;
+using DigitalBanking.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace DigitalBanking.WebApi.Tests.IntegrationTests.Customers
+{
+    public class GetCustomerById : WebApplicationFactory<Program>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+
+        public GetCustomerById(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient();
+        }
+
+        //private async Task SeedData(Guid customerId)
+        //{
+        //    using var scope = _factory.Services.CreateScope();
+        //    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        //    var customer = Customer.Create("test", "test", "test", "test", "test");
+        //    dbContext.Customers.Add();
+        //}
+    }
+}
