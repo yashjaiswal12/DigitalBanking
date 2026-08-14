a8471d17686dc7cc140475641e8143f571a0c9c1
yashjaiswal <yashjaiswal985@gmail.com>
2026-08-15 01:13:49 +0530

    improvements

--- DigitalBanking.Infrastructure/Repositories/RefreshTokenRepository.cs
+++ DigitalBanking.Infrastructure/Repositories/RefreshTokenRepository.cs
@@ -26,8 +26,8 @@ namespace DigitalBanking.Infrastructure.Repositories
 
         public async Task<RefreshToken?> GetRefreshTokenByCustomerIdAsync(Guid id, CancellationToken cancellationToken)
         {
-            return await _context.RefreshTokens.Where(x => x.CustomerId == id && !x.IsRevoked)
-                .OrderByDescending(x => x.RevokedOn).FirstOrDefaultAsync(cancellationToken);
+            return await _context.RefreshTokens.Where(x => x.CustomerId == id && x.RevokedOn == null)
+                .OrderByDescending(x => x.CreatedOn).FirstOrDefaultAsync(cancellationToken);
         }
 
         public async Task<List<RefreshToken>> GetRefreshTokensByCustomerIdAsync(Guid id, CancellationToken cancellationToken)
